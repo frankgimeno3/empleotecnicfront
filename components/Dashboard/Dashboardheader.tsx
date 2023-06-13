50df70bb70d10db56567df1c23250b9882a227ce
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-13 20:37:30 +0200

    repository change

---
+++ components/Dashboard/Dashboardheader.tsx
@@ -0,0 +1,15 @@
+
+
+const Dashboardheader = () => {
+  return (
+    <div className="mt-20  display-flex flex-row">
+            <button className="p-5 bg-gray-100"> Notificaciones </button>
+            <button className="p-5 bg-gray-100"> Notificaciones </button>
+            <button className="p-5 bg-gray-100"> Notificaciones </button>
+            <button className="p-5 bg-gray-100"> Notificaciones </button>
+        
+    </div>
+  );
+};
+
+export default Dashboardheader;
---
+++ pages/dashboard.tsx
@@ -0,0 +1,11 @@
+import Notificaciones from "../components/Dashboard/Notificaciones";
+import Dashboardheader from "../components/Dashboard/Dashboardheader";
+
+export default function Dashboard() {
+    return (
+            <>
+            <Dashboardheader/>
+            <Notificaciones />
+        </>
+    )
+  }
