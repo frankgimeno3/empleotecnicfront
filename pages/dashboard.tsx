a2ed877548020f7a69bc81cd68d299ffa8650dc3
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-19 14:13:11 +0200

    corrijo link nav, añado estilos al dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -58,14 +58,14 @@ export default function Dashboard() {
   return (
     <>
       <nav className="fixed top-0 left-0 right-0 z-10 bg-cyan-950 bg-opacity-25 backdrop-filter backdrop-blur-lg flex items-center justify-between p-4 md:text-xs xl:text-base ">
-        <Link href="/" passHref>
+        <Link href="/dashboard" passHref>
           <button className="bg-opacity-25 text-white rounded-full py-2 px-4 border-2 hover:bg-white hover:text-cyan-950  hover:cursor-pointer">
             E
           </button>
         </Link>
       </nav>
       <div style={{ margin: "0 20vw" }}>
-        <div className="flex flex-col items-center  font-mono text-sm bg-gradient-to-r from-gray-700 to-cyan-950 ">
+        <div className="flex flex-col items-center  font-mono text-sm bg-gradient-to-r from-gray-700 to-cyan-950 mt-20">
           {isSMorMD && (
             <div className="relative" ref={menuRef}>
               <Image
