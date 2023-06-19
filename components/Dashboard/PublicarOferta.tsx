6c4389180abbe8e65271d96b653ed675c7e6af24
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-19 14:34:04 +0200

    aplico espaciado publicaroferta

--- components/Dashboard/PublicarOferta.tsx
+++ components/Dashboard/PublicarOferta.tsx
@@ -2,7 +2,7 @@
  
 const PublicarOferta = () => {
     return (
-      <div className="bg-gray-200 mx-10vw w-full">
+      <div className="mt-10 p-20 bg-gray-200 mx-10vw w-full">
         <h1 className="text-black">Este es el contenido de Publicar Oferta</h1>
        
       </div>
