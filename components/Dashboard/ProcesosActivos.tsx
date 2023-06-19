faaeab1038f067b03501f4e0ca3b9949b9687803
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-19 15:13:20 +0200

    mejoro precisión borde superior procesos activos

--- components/Dashboard/ProcesosActivos.tsx
+++ components/Dashboard/ProcesosActivos.tsx
@@ -2,9 +2,10 @@
  
 const ProcesosActivos = () => {
     return (
-      <div className="mt-20 p-20 bg-gray-200 mx-10vw w-full">
-        <h1 className="text-black">Este es el contenido de la Procesos Activos</h1>
-       
+      <div className="mt-6 mx-10vw w-full">
+        <div className="mt-10 p-20 bg-gray-200 ">
+          <h1 className="text-black">Este es el contenido de la Procesos Activos</h1>
+        </div>
       </div>
     );
   };
