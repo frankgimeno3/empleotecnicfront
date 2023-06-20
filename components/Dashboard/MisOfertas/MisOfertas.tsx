6ada55f89668f23bccfea7b3671f438549f27e3a
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-20 21:43:25 +0200

    añado titular mis ofertas

--- components/Dashboard/MisOfertas/MisOfertas.tsx
+++ components/Dashboard/MisOfertas/MisOfertas.tsx
@@ -5,6 +5,7 @@ const MisOfertas = () => {
     return (
       <div className='mt-6'>
         < div className=" mt-10 p-20 bg-gray-200 pt-1">
+        <h1 className="text-3xl">Ofertas publicadas</h1>
         <MyOfferCard />
         <MyOfferCard />
         <MyOfferCard />
