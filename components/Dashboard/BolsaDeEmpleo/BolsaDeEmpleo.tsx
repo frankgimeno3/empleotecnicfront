ff20564dea9afa56760fbe57bd07cd4a446ca2cb
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-20 21:46:04 +0200

    añado titular bolsa de empleo

--- components/Dashboard/BolsaDeEmpleo/BolsaDeEmpleo.tsx
+++ components/Dashboard/BolsaDeEmpleo/BolsaDeEmpleo.tsx
@@ -6,6 +6,7 @@ const BolsaDeEmpleo = () => {
   return (
     <div className="mt-6 pb-20 mx-20vw w-full">
       <div className="bg-white mt-10 py-5">
+        <h1 className="text-3xl font-bold pt-5 flex justify-center">Bolsa de empleo</h1>
         <QueYDonde />
         <FormularioBusquedaAvanzada />
       </div>
