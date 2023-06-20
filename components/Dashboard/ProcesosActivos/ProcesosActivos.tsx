0ccf4dded653b62f4fa1fad65e01080e0e2ac286
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-20 12:07:34 +0200

    añado contenido procesos activos

--- components/Dashboard/ProcesosActivos/ProcesosActivos.tsx
+++ components/Dashboard/ProcesosActivos/ProcesosActivos.tsx
@@ -1,22 +1,41 @@
- 
- 
-import TarjetaCandidato from './TarjetaCandidato'
+import TarjetaCandidato from "./TarjetaCandidato";
 
 const ProcesosActivos = () => {
-    return (
-      <div className="mt-6 mx-10vw w-full">
-        <div className="mt-10 p-20 bg-gray-200 ">
-          <h1 className="text-black">Este es el contenido de la Procesos Activos</h1>
-          <TarjetaCandidato/>
-          <TarjetaCandidato/>
-          <TarjetaCandidato/>
-          <TarjetaCandidato/>
-          <TarjetaCandidato/>
-          <TarjetaCandidato/>
-          <TarjetaCandidato/>
+  return (
+    <div className="mt-6 mx-10vw w-full">
+      <div className="mt-10 p-20 bg-gray-200 ">
+        <h1 className="text-black text-3xl">
+          Procesos Activos
+        </h1>
+        <div className="bg-gray-100 mt-10 p-5">
+          <h2 className="text-black text-2xl">Oferta 1</h2>
+          <div className="flex flex-wrap">
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+          </div>
+        </div>
+        <div className="bg-gray-100 mt-10 p-5">
+          <h2 className="text-black text-2xl">Oferta 2</h2>
+          <div className="flex flex-wrap">
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+            <TarjetaCandidato />
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default ProcesosActivos;
+    </div>
+  );
+};
+
+export default ProcesosActivos;
--- components/Dashboard/ProcesosActivos/TarjetaCandidato.tsx
+++ components/Dashboard/ProcesosActivos/TarjetaCandidato.tsx
@@ -1,13 +1,37 @@
- 
- 
+import Image from 'next/image';
+
 const TarjetaCandidato = () => {
-    return (
-      <div className="mt-6 mx-10vw w-full">
-        <div className="mt-10 p-20 bg-gray-200 ">
-          <h1 className="text-black">Este es el contenido de la Procesos Activos</h1>
+  return (
+    <div className="mt-6 mx-8 flex " style={{ maxWidth: '30vw' }} >
+      <div className="bg-white shadow-md rounded-lg overflow-hidden">
+        <div>
+          <Image src="/images/candidato.jpg" alt="Foto de candidato" className="w-full h-64 object-cover" width={600} height={600} />
+        </div>
+        <div className="p-6">
+          <h1 className="text-xl font-semibold mb-2">Nombre del Candidato</h1>
+          <p className="text-gray-700">Descripción del candidato</p>
+          <div className="flex items-center mt-4">
+            <svg className="w-6 h-6 text-gray-600 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M12 19l9 2-9-18-9 18 9-2zm0 0v-8" />
+            </svg>
+            <p className="text-gray-600">Ciudad, País</p>
+          </div>
+          <div className="flex items-center mt-2">
+            <svg className="w-6 h-6 text-gray-600 mr-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+              <path d="M12 20h9" />
+              <path d="M4 20h4" />
+              <path d="M9 16v4" />
+              <path d="M15 16v4" />
+              <path d="M9 12h6" />
+              <path d="M12 6l2 4h-4l2-4zm2 10v-6" />
+              <path d="M12 6v6" />
+            </svg>
+            <p className="text-gray-600">Experiencia: 5 años</p>
+          </div>
         </div>
       </div>
-    );
-  };
-  
-  export default TarjetaCandidato;
+    </div>
+  );
+};
+
+export default TarjetaCandidato;
