d35fe542706ce5f2fad3d9cc2e6f49fe1d460a1b
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-20 11:29:08 +0200

    añado tarjetas candidato procesos activos

--- components/Dashboard/ProcesosActivos/ProcesosActivos.tsx
+++ components/Dashboard/ProcesosActivos/ProcesosActivos.tsx
@@ -1,10 +1,19 @@
  
  
+import TarjetaCandidato from './TarjetaCandidato'
+
 const ProcesosActivos = () => {
     return (
       <div className="mt-6 mx-10vw w-full">
         <div className="mt-10 p-20 bg-gray-200 ">
           <h1 className="text-black">Este es el contenido de la Procesos Activos</h1>
+          <TarjetaCandidato/>
+          <TarjetaCandidato/>
+          <TarjetaCandidato/>
+          <TarjetaCandidato/>
+          <TarjetaCandidato/>
+          <TarjetaCandidato/>
+          <TarjetaCandidato/>
         </div>
       </div>
     );
