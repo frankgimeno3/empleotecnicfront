7c121a4415801a888cd126c621aecb465d48c19d
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-22 01:35:58 +0200

    mejoro márgenes ofertas

--- components/Dashboard/MisOfertas/MisOfertas.tsx
+++ components/Dashboard/MisOfertas/MisOfertas.tsx
@@ -3,9 +3,9 @@ import MyOfferCard from './MyOfferCard'
 
 const MisOfertas = () => {
     return (
-      <div className='mt-6'>
-        < div className=" mt-10 p-20 bg-gray-200 pt-1">
-        <h1 className="text-3xl mt-5 ml-20">Ofertas publicadas</h1>
+      <div className='mt-6 mx-20 bg-gradient-to-r from-gray-700 to-cyan-950' >
+        < div className=" mt-10 py-20 bg-gray-200 pt-1">
+        <h1 className="text-3xl mt-10 ml-20 font-bold">Ofertas publicadas</h1>
         <MyOfferCard />
         <MyOfferCard />
         <MyOfferCard />
