54980c2e06749ab4988451d321b08cf9fa423be3
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-23 18:41:20 +0200

    le meto un Head

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ import MainHeader from '../components/Home/MainHeader'
 import BolsasDeEmpleo from '../components/Home/BolsasDeEmpleo'
 import SobreNosotros from '../components/Home/SobreNosotros'
 import Navbar from '../components/Navbar/Navbar'
+import Head from 'next/head'
+
 export default function Home() {
   return (
+    <>
+            <Head>
+            <title>Empleotecnic</title>
+            <meta name="Empleotecnic" content="Bolsa de empleo especializada en perfiles técnicos para el sector del vidrio, carpintería y protección solar" />        
+            </Head>
+
       <div>      
       <Navbar />
       <div className="flex flex-col items-center  font-mono text-sm bg-gradient-to-r from-gray-700 to-cyan-950 ">
@@ -14,5 +22,11 @@ export default function Home() {
         <SobreNosotros/>
       </div>
       </div>
+      </>
   )
+}
+
+
+export const getServerSideProps = async (context)=>{
+
 }
