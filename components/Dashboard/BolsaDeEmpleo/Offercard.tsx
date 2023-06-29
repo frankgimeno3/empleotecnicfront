bd851417b8c89bf520a0f1e50e838181c34a5c48
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-29 18:46:23 +0200

    ahora hacer click en boton abre solicitud

--- components/Dashboard/BolsaDeEmpleo/Offercard.tsx
+++ components/Dashboard/BolsaDeEmpleo/Offercard.tsx
@@ -39,7 +39,7 @@ const OfferCard: React.FC<OfferCardProps> = ({ oferta }) => {
           </li>
         </ul>
         <a
-          href="#"
+          href="/dashboard/solicitud"
           className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
         >
           Aplicar ahora
