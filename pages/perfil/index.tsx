cbdd4a6d75b38e6a6f86780318ce5b21fdcad38d
Frank Gimeno <frankgimeno3@gmail.com>
2023-06-30 13:44:21 +0200

    corrijo importacion errónea de navbar al perfil

--- pages/perfil/index.tsx
+++ pages/perfil/index.tsx
@@ -1,28 +1,10 @@
 "use client";
 import { useSession, signOut } from "next-auth/react";
-import Navigation, { NavigationProps } from "../../components/Navbar/NavbarIn";
+import Navbar from "../../components/Navbar/NavbarIn";
 
 
 
-  const handleLogout = async () => {
-    try {
-      const res = await fetch("http://localhost:5000/auth/logout", {
-        method: "POST",
-        credentials: "include", // Para enviar las cookies al servidor
-      });
-
-      if (res.status === 200) {
-        // Se ha cerrado sesión con éxito
-        router.push("/login"); // Redirige al usuario a la página de inicio de sesión
-      } else {
-        // Manejar el caso de error si no se pudo cerrar sesión
-        console.error("No se pudo cerrar sesión");
-      }
-    } catch (error) {
-      console.error("Error al cerrar sesión", error);
-    }
-    router.push("/");
-  };
+ 
 
 function ProfilePage() {
   const { data: session, status } = useSession();
@@ -31,11 +13,8 @@ function ProfilePage() {
 
   return (
     <>
-      <Navigation
-        selectedButton={selectedButton}
-        onButtonClick={handleButtonClick}
-        onLogout={handleLogout}
-      />    <div className="h-[calc(100vh-4rem)] flex flex-col gap-y-10 items-center justify-center">
+      <Navbar/>
+      <div className="h-[calc(100vh-4rem)] flex flex-col gap-y-10 items-center justify-center">
       <h1 className="font-bold text-3xl">Profile</h1>
 
       <pre className="bg-zinc-800 p-4">
